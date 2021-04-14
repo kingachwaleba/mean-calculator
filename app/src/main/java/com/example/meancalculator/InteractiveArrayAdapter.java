362c9fcc6d42e3ea39ce06dd5d9fb29613e7906f
kingachwaleba <kingach6@gmail.com>
2021-04-14 13:23:44 +0200

    Edited methods

--- app/src/main/java/com/example/meancalculator/InteractiveArrayAdapter.java
+++ app/src/main/java/com/example/meancalculator/InteractiveArrayAdapter.java
@@ -19,20 +19,24 @@ public class InteractiveArrayAdapter extends RecyclerView.Adapter<InteractiveArr
         this.layoutInflater = layoutInflater;
     }
 
+    // Create the main layout element and create a holder for the given row
     @NonNull
     @Override
     public GradesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View line = layoutInflater.inflate(R.layout.list_line, null);
+        return new GradesViewHolder(line);
     }
 
+    // Populate a row, stored in a holder, with data for the specified row
     @Override
     public void onBindViewHolder(@NonNull GradesViewHolder holder, int position) {
-
+        GradesActivity grade = gradesActivityList.get(position);
     }
 
+    // Return the number of items of the list
     @Override
     public int getItemCount() {
-        return 0;
+        return gradesActivityList.size();
     }
 
     public class GradesViewHolder extends RecyclerView.ViewHolder {
