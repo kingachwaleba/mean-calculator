1afb27f92dd1ff8300a42cc80a8cfa360f07d8bc
kingachwaleba <kingach6@gmail.com>
2021-04-14 13:10:27 +0200

    Implemented methods

--- app/src/main/java/com/example/meancalculator/InteractiveArrayAdapter.java
+++ app/src/main/java/com/example/meancalculator/InteractiveArrayAdapter.java
@@ -1,4 +1,31 @@
 package com.example.meancalculator;
 
-public class InteractiveArrayAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class InteractiveArrayAdapter extends RecyclerView.Adapter<InteractiveArrayAdapter.GradesViewHolder> {
+    @NonNull
+    @Override
+    public GradesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GradesViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class GradesViewHolder extends RecyclerView.ViewHolder {
+        public GradesViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
