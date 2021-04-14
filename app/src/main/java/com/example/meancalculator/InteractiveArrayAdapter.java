c2f24c0ff73e11be0f20ffaa39d2ff1ed7bb6a7b
kingachwaleba <kingach6@gmail.com>
2021-04-14 13:12:53 +0200

    Created constructor

--- app/src/main/java/com/example/meancalculator/InteractiveArrayAdapter.java
+++ app/src/main/java/com/example/meancalculator/InteractiveArrayAdapter.java
@@ -1,12 +1,24 @@
 package com.example.meancalculator;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class InteractiveArrayAdapter extends RecyclerView.Adapter<InteractiveArrayAdapter.GradesViewHolder> {
+
+    private List<GradesActivity> gradesActivityList;
+    private LayoutInflater layoutInflater;
+
+    public InteractiveArrayAdapter(List<GradesActivity> gradesActivityList, LayoutInflater layoutInflater) {
+        this.gradesActivityList = gradesActivityList;
+        this.layoutInflater = layoutInflater;
+    }
+
     @NonNull
     @Override
     public GradesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
