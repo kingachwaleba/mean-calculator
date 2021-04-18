d21445bb84ec82bb080740788092b75cb996d01d
kingachwaleba <kingach6@gmail.com>
2021-04-18 16:38:23 +0200

    Added button and RecycleView element

--- app/src/main/java/com/example/meancalculator/GradesActivity.java
+++ app/src/main/java/com/example/meancalculator/GradesActivity.java
@@ -1,10 +1,20 @@
 package com.example.meancalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class GradesActivity extends AppCompatActivity {
+
+    Button button;
+    RecyclerView recyclerView;
+
+    public GradesActivity(Button button, RecyclerView recyclerView) {
+        this.button = button;
+        this.recyclerView = recyclerView;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
