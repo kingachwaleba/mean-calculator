13da5f8d5cdce3146fe664f1aa3136101627b5cc
kingachwaleba <kingach6@gmail.com>
2021-04-18 17:14:14 +0200

    Added fields, constructor, setters ang getters

--- app/src/main/java/com/example/meancalculator/GradeModel.java
+++ app/src/main/java/com/example/meancalculator/GradeModel.java
@@ -1,4 +1,35 @@
 package com.example.meancalculator;
 
 public class GradeModel {
+
+    private String name;
+    private int value;
+    private final int id;
+    static int count = 0;
+
+    public GradeModel(String name) {
+        this.name = name;
+        this.id = count;
+        count++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
