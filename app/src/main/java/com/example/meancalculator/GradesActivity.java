bdab26530c68118d40568001234e716ca4e36365
kingachwaleba <kingach6@gmail.com>
2021-04-20 18:04:54 +0200

    Calculated the mean and returned the result

--- app/src/main/java/com/example/meancalculator/GradesActivity.java
+++ app/src/main/java/com/example/meancalculator/GradesActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -46,5 +47,25 @@ public class GradesActivity extends AppCompatActivity {
 
         // Set the layout that set the elements in RecyclerView
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        button = findViewById(R.id.countButton);
+
+        button.setOnClickListener(v -> {
+            Bundle bundle2 = new Bundle();
+            bundle2.putDouble("srednia", calculateMean());
+            Intent intent = new Intent();
+            intent.putExtras(bundle2);
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+    }
+
+    public double calculateMean() {
+        double sum = 0;
+        for (GradeModel element : gradeModelArrayList) {
+            int grade = element.getValue();
+            sum += grade;
+        }
+        return sum/(double) gradeModelArrayList.size();
     }
 }
