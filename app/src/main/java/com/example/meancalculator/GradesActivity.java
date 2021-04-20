e0056f0168da7a051a45b8edde752162402fd133
kingachwaleba <kingach6@gmail.com>
2021-04-20 17:25:55 +0200

    Added grades and names of subjects to anarraylist

--- app/src/main/java/com/example/meancalculator/GradesActivity.java
+++ app/src/main/java/com/example/meancalculator/GradesActivity.java
@@ -6,10 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class GradesActivity extends AppCompatActivity {
 
     Button button;
     RecyclerView recyclerView;
+    ArrayList<GradeModel> gradeModelArrayList;
 
     public GradesActivity(Button button, RecyclerView recyclerView) {
         this.button = button;
@@ -20,5 +23,17 @@ public class GradesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grades);
+
+        gradeModelArrayList = new ArrayList<>();
+
+        String[] gradesList = getResources().getStringArray(R.array.gradeTable);
+
+        Bundle bundle = getIntent().getExtras();
+        final int gradesNumber = bundle.getInt("gradesInput");
+
+        for (int i = 0; i < gradesNumber; i++) {
+            GradeModel grade = new GradeModel(gradesList[i]);
+            gradeModelArrayList.add(grade);
+        }
     }
 }
